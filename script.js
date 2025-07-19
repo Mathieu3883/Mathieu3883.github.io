fc4d346bf90017d70f6eb0174e64955f5457f12d
Mathieu Geus <mathieugeus@yahoo.fr>
2025-07-19 22:49:10 +0200

    feat: update toggle functionality for 'Voir plus' buttons with scrollHeight

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ voirPlusButtons.forEach(button => {
     const targetId = button.getAttribute("data-target");
     const targetContent = document.getElementById(targetId);
     targetContent.classList.toggle("open");
+    targetContent.style.maxHeight = button.textContent === "Voir plus" ? targetContent.scrollHeight + "px" : "0";
     button.textContent = button.textContent === "Voir plus" ? "Voir moins" : "Voir plus";
   });
 });
